8a75dc978c7f6284949b4a0957001fffc96c2c1f
Chaynoi Naysai <Chaynoy_naysai@hotmail.co.th>
2021-06-10 23:23:08 +0700

    connect cloud mongodb && Register

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+
+const URI = 'mongodb+srv://admin_khunchay:admin1234@cluster0.sspfa.mongodb.net/social?retryWrites=true&w=majority'
+
+const connectDB = async () => {
+   await mongoose.connect(URI, 
+        {useNewUrlParser: true, useUnifiedTopology: true },
+        ()=> {
+            console.log("connceted to MongoDB")
+        }
+    );
+}
+
+//midelware
+app.use(express.json())
+app.use(helmet());
+app.use(morgan("common"))
+
+app.use("/api/users", userRouter)
+app.use("/api/auth", authRouter)
+
+connectDB()
+
+app.listen(8800,()=>{
+    console.log("backennd server is running!")
+})
